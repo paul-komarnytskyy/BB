f21287330fc8c32ca9c31c9c7c5b31f3a6830d08
Paul Komarnytskyy <paul.komarnytskyy@gmail.com>
2017-10-21 21:19:49 +0300

    adding new and updating some old components

---
+++ BB.Web/app/Components/home.component.ts
@@ -0,0 +1,8 @@
+﻿import { Component } from '@angular/core';
+//import { NavbarComponent } from './navbar.component'
+
+@Component({
+    selector: 'my-app',
+    templateUrl: 'app/Components/home.html',
+})
+export class HomeComponent  { name = 'Angular'; }
---
+++ BB.Web/app/Components/page-not-found.component.ts
@@ -0,0 +1,9 @@
+﻿import { Component } from '@angular/core';
+
+@Component({
+    moduleId: module.id,
+    template: '<h1>Page not found</h1>',
+})
+
+export class PageNotFoundComponent {
+}
