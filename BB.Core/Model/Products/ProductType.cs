2a5d65a4e3518a6673a87a44142effaad831ead7
Paul Komarnytskyy <paul.komarnytskyy@gmail.com>
2017-10-22 15:33:23 +0300

    added product characteristics to product type

--- BB.Core/Model/Products/ProductType.cs
+++ BB.Core/Model/Products/ProductType.cs
@@ -1,9 +1,13 @@
-﻿namespace BB.Core.Model.Products
+﻿using System.Collections.Generic;
+
+namespace BB.Core.Model.Products
 {
     public class ProductType
     {
         public string Name { get; set; }
 
         public ProductType ParentType { get; set; }
+
+        public List<string> AvailableCharacteristics { get; set; }
     }
 }
