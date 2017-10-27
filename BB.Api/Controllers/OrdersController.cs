6030be88c41afc744d4e2d3e9c971efb7125c3b9
Markiyan Oliskevych <markian.oliskevych@gmail.com>
2017-10-27 03:10:38 +0300

    Order controller fully operational

--- BB.Api/Controllers/OrdersController.cs
+++ BB.Api/Controllers/OrdersController.cs
@@ -9,6 +9,8 @@ using System.Net.Http;
 using System.Web.Http;
 using System.Web.Http.Description;
 using BB.Core;
+using BB.Api.DTO;
+using BB.Api.Models;
 using BB.Core.Model;
 
 namespace BB.Api.Controllers
@@ -17,8 +19,7 @@ namespace BB.Api.Controllers
     {
         private BBEntities db = new BBEntities();
 
-        [HttpGet]
-        [Route("api/Orders/getOrders")]
+        [Route("api/Orders/ordersList")]
         public IHttpActionResult GetOrders(long? userId, int? pageNumb, int? pageSize)
         {
             var orders = db.Orders.AsQueryable();
@@ -31,10 +32,9 @@ namespace BB.Api.Controllers
             {
                 orders = orders.Take(pageSize.Value).Skip(pageSize.Value * (pageNumb.Value - 1));
             }
-            return Ok(new { result = orders, totalItemsCount = count, pageNumb, pageSize });
+            return Ok(new { result = orders.ToList().Select(it => it.ConvertToDTO()), totalItemsCount = count, pageNumb, pageSize });
         }
 
-        [HttpGet]
         [Route("api/Orders/createOrder")]
         public IHttpActionResult CreateOrder(long userId)
         {
@@ -43,12 +43,123 @@ namespace BB.Api.Controllers
             {
                 return Ok("No user found");
             }
-           
-            var newOrder = new Order();
+
+            var status = new BB.Core.Model.StatusUpdate();
+            status.Date = DateTime.Now;
+            status.Status = Status.Cart;
+
+            var newOrder = new BB.Core.Model.Order();
             newOrder.User = user;
+            newOrder.StatusUpdates.Add(status);
             db.Orders.Add(newOrder);
             db.SaveChanges();
-            return Ok(newOrder);
+            return Ok(new { order = newOrder.ConvertToDTO() });
+        }
+
+        [Route("api/Orders/updateOrder")]
+        public IHttpActionResult GetUpdateOrder(Guid orderId, long productId, int? ammount)
+        {
+            if (ammount.HasValue && ammount.Value == 0)
+            {
+                return Ok("Cannot order zero items");
+            }
+            var order = db.Orders.FirstOrDefault(it => it.OrderId == orderId);
+            if (order == null)
+            {
+                return Ok("No order found");
+            }
+            if (order.ConvertToDTO().Status != Status.Cart)
+            {
+                return Ok("Order is already completed");
+            }
+            var product = db.Products.FirstOrDefault(it => it.ProductId == productId);
+            if (order == null)
+            {
+                return Ok("No product found");
+            }
+            var orderItem = order.OrderItems.FirstOrDefault(it => it.ProductId == productId);
+            if (orderItem != null)
+            {
+                orderItem.Count = ammount.HasValue ? ammount.Value : 1;
+                return Ok(new { order = order.ConvertToDTO() });
+            }
+
+            var newOrder = new BB.Core.Model.OrderItem();
+            newOrder.Order = order;
+            newOrder.Product = product;
+            newOrder.Count = ammount.HasValue ? ammount.Value : 1;
+            newOrder.PricePerItem = product.Price;
+            db.OrderItems.Add(newOrder);
+            db.SaveChanges();
+
+            return Ok(new { order = newOrder.Order.ConvertToDTO() });
+        }
+
+        [Route("api/Orders/removeProduct")]
+        public IHttpActionResult GetRemoveProduct(Guid orderId, long productId)
+        {
+            var order = db.Orders.FirstOrDefault(it => it.OrderId == orderId);
+            if (order == null)
+            {
+                return Ok("No order found");
+            }
+            if (order.ConvertToDTO().Status != Status.Cart)
+            {
+                return Ok("Order is already completed");
+            }
+            var product = db.Products.FirstOrDefault(it => it.ProductId == productId);
+            if (order == null)
+            {
+                return Ok("No product found");
+            }
+            var orderItem = order.OrderItems.FirstOrDefault(it => it.ProductId == productId);
+            if (orderItem == null)
+            {
+               return Ok("No such product in cart");
+            }
+            db.OrderItems.Remove(orderItem);
+            db.SaveChanges();
+
+            return Ok(new { order = order.ConvertToDTO() });
+        }
+
+        [Route("api/Orders/updateOrderStatus")]
+        public IHttpActionResult GetUpdateOrderStatus(Guid orderId, Status status)
+        {
+            var order = db.Orders.FirstOrDefault(it => it.OrderId == orderId);
+            if (order == null)
+            {
+                return Ok("No order found");
+            }
+
+            var newStatus = new BB.Core.Model.StatusUpdate();
+            newStatus.Date = DateTime.Now;
+            newStatus.Status = status;
+
+            order.StatusUpdates.Add(newStatus);
+            db.SaveChanges();
+
+            return Ok(new { order = order.ConvertToDTO() });
+        }
+
+        [Route("api/Orders/deleteOrder")]
+        public IHttpActionResult DeleteOrder(Guid orderId)
+        {
+            var order = db.Orders.FirstOrDefault(it => it.OrderId == orderId);
+            if (order == null)
+            {
+                return Ok("No order found");
+            }
+            if (order.ConvertToDTO().Status != Status.Cart)
+            {
+                return Ok("Order is already completed");
+            }
+
+            db.OrderItems.RemoveRange(order.OrderItems);
+            db.StatusUpdates.RemoveRange(order.StatusUpdates);
+            db.Orders.Remove(order);
+            db.SaveChanges();
+            return Ok("Success");
         }
     }
 }
