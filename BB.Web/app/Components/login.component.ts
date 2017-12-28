2bf1944e77b9457baccadc07b43c844881b28b53
Paul Komarnytskyy <paul.komarnytskyy@gmail.com>
2017-12-28 02:04:19 +0200

    changed app title added failure handling for login component

--- BB.Web/app/Components/login.component.ts
+++ BB.Web/app/Components/login.component.ts
@@ -1,8 +1,9 @@
-﻿import { Component, Output, EventEmitter } from '@angular/core';
+﻿import { Component, ViewChild, Output, EventEmitter } from '@angular/core';
 import { User } from '../Model/Users/User';
 import { Router } from '@angular/router'
 import { AuthenticationService } from '../Services/authentication.service'
 import { UserService } from '../Services/user.service'
+
 
 @Component({
     selector: 'login',
@@ -14,39 +15,42 @@ export class LoginComponent {
     private username: string = '';
     private password: string = '';
     private isAuthenticated: boolean;
-
+    private incorrectCredentials: boolean;
 
     @Output()
     statusChanged: EventEmitter<boolean> = new EventEmitter<boolean>();
 
     constructor(private router: Router, private authenticationService: AuthenticationService, private userService: UserService) {
         this.isAuthenticated = this.authenticationService.token != null;
+        this.statusChanged.emit(this.isAuthenticated);
+        this.incorrectCredentials = false;
     }
 
     login() {
         var name = this.username;
         this.authenticationService.login(name, this.password)
             .subscribe((res) => {
-                    var token = res.json();
-                    if (token) {
-                        // set token property
-                        this.authenticationService.token = token.access_token;
+                var token = res.json();
+                if (token) {
+                    // set token property
+                    this.authenticationService.token = token.access_token;
 
-                        // store username and jwt token in local storage to keep user logged in between page refreshes
-                        localStorage.setItem('currentUser', JSON.stringify({ username: name, token: token }));
+                    // store username and jwt token in local storage to keep user logged in between page refreshes
+                    localStorage.setItem('currentUser', JSON.stringify({ username: name, token: token }));
 
-                        this.userService.CurrentUser = new User(name, null, null, null);
-                        // return true to indicate successful login
-                        this.isAuthenticated = this.authenticationService.token != null;
-                        this.username = '';
-                        this.password = '';
-                        this.authenticationService.getUserID();
-                        this.statusChanged.emit(this.isAuthenticated);
-                    }
-                },
-                (err) => ),
-                () => {
-                });
+                    this.userService.CurrentUser = new User(name, null, null, null);
+                    // return true to indicate successful login
+                    this.isAuthenticated = this.authenticationService.token != null;
+                    this.username = '';
+                    this.password = '';
+                    this.authenticationService.getUserID();
+                    this.statusChanged.emit(this.isAuthenticated);
+                    this.incorrectCredentials = false;
+                }
+            },
+            (err) => this.incorrectCredentials = true,
+            () => {
+            });
     }
 
     logout() {
