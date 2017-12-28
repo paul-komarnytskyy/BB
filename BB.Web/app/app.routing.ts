3e45a0598be25bf1428cdf798e5941fb64cc70d5
Paul Komarnytskyy <paul.komarnytskyy@gmail.com>
2017-12-28 09:27:32 +0200

    included details component and service into routing, modules and .csproj

--- BB.Web/app/app.routing.ts
+++ BB.Web/app/app.routing.ts
@@ -4,6 +4,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './Components/home.component';
 import { ProductListComponent } from './Components/product-list.component';
 import { CategoryListComponent } from './Components/category-list.component';
+import { DetailsComponent } from './Components/details.component';
 import { RegisterComponent } from './Components/register.component';
 import { PageNotFoundComponent } from './Components/page-not-found.component';
 
@@ -15,6 +16,7 @@ const appRoutes: Routes = [
     { path: 'products', component: ProductListComponent },
     { path: 'categories', component: CategoryListComponent },
     { path: 'register', component: RegisterComponent },
+    { path: 'details/:id', component: DetailsComponent },
     { path: 'user-details', component: UserDetailsComponent, canActivate: [AuthGuard] },
     { path: '', redirectTo: '/home', pathMatch: 'full' },
     { path: '**', component: PageNotFoundComponent }
