5068b354648236e53f594be68d2c21d9b69807bf
Paul Komarnytskyy <paul.komarnytskyy@gmail.com>
2018-01-17 00:27:34 +0200

    Added cors to api added AddItemToCart universal method Added cors to details service

--- BB.Web/app/Components/details.component.ts
+++ BB.Web/app/Components/details.component.ts
@@ -4,6 +4,7 @@ import { DetailsService } from '../Services/details.service'
 import { Product } from '../Model/Products/Product';
 import { Router } from '@angular/router';
 import { OrdersService } from '../Services/orders.service';
+import { AuthenticationService } from '../Services/authentication.service';
 import o = require("../Model/Orders/Order");
 import Order = o.Order;
 
@@ -15,7 +16,7 @@ export class DetailsComponent implements OnInit {
     private Id: any;
     private product: Product;
     private order: Order;
-    constructor(private activatedRoute: ActivatedRoute, private detailsService: DetailsService, private router: Router, private orderService: OrdersService) {
+    constructor(private activatedRoute: ActivatedRoute, private detailsService: DetailsService, private router: Router, private orderService: OrdersService, private authenticationService: AuthenticationService) {
         let params: any = this.activatedRoute.snapshot.params;
         this.Id = params.id;
         this.product = new Product();
@@ -32,8 +33,7 @@ export class DetailsComponent implements OnInit {
     }
 
    addToCart() {
-       debugger;
-       this.orderService.createOrder(1).map((response) => response.json())
+       this.orderService.addItemToOrder(this.authenticationService.userID, this.Id).map((response) => response.json())
            .subscribe((data) => {
                this.order = data
                console.log(data);
--- BB.Web/app/Services/details.service.ts
+++ BB.Web/app/Services/details.service.ts
@@ -18,6 +18,7 @@ export class DetailsService {
 
         let headers = new Headers({ 'Content-Type': 'application/json' });
         headers.append('Authorization', 'Bearer ' + this.authenticationService.token);
+        headers.append('Access-Control-Allow-Origin', '*');
         let requestOptions = new RequestOptions({ headers: headers });
 
         var observable = this.http.get(this.basePath + '/api/products/GetProductById/' + Id, requestOptions);
